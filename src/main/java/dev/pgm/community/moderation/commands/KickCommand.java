391cf61dccab564d469104565354e54b04154b77
applenick <applenick@users.noreply.github.com>
2020-12-30 00:16:39 -0800

    Adjust warn and mute to prevent dead target bug
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- src/main/java/dev/pgm/community/moderation/commands/KickCommand.java
+++ src/main/java/dev/pgm/community/moderation/commands/KickCommand.java
@@ -6,30 +6,39 @@ import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Dependency;
 import co.aikar.commands.annotation.Description;
 import co.aikar.commands.annotation.Syntax;
-import co.aikar.commands.bukkit.contexts.OnlinePlayer;
 import dev.pgm.community.CommunityCommand;
 import dev.pgm.community.CommunityPermissions;
 import dev.pgm.community.moderation.feature.ModerationFeature;
 import dev.pgm.community.moderation.punishments.PunishmentType;
+import dev.pgm.community.users.feature.UsersFeature;
 import dev.pgm.community.utils.CommandAudience;
 
 public class KickCommand extends CommunityCommand {
 
   @Dependency private ModerationFeature moderation;
+  @Dependency private UsersFeature usernames;
 
   @CommandAlias("kick|k")
   @Description("Kick a player from the server")
   @Syntax("[player] [reason]")
   @CommandCompletion("@players")
   @CommandPermission(CommunityPermissions.KICK)
-  public void kick(CommandAudience audience, OnlinePlayer target, String reason) {
-    moderation.punish(
-        PunishmentType.KICK,
-        target.getPlayer().getUniqueId(),
-        audience,
-        reason,
-        null,
-        false,
-        isVanished(audience));
+  public void kick(CommandAudience audience, String target, String reason) {
+    getTarget(target, usernames)
+        .thenAccept(
+            id -> {
+              if (id.isPresent()) {
+                moderation.punish(
+                    PunishmentType.KICK,
+                    id.get(),
+                    audience,
+                    reason,
+                    null,
+                    false,
+                    isVanished(audience));
+              } else {
+                audience.sendWarning(formatNotFoundComponent(target));
+              }
+            });
   }
 }
--- src/main/java/dev/pgm/community/moderation/commands/WarnCommand.java
+++ src/main/java/dev/pgm/community/moderation/commands/WarnCommand.java
@@ -6,30 +6,39 @@ import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Dependency;
 import co.aikar.commands.annotation.Description;
 import co.aikar.commands.annotation.Syntax;
-import co.aikar.commands.bukkit.contexts.OnlinePlayer;
 import dev.pgm.community.CommunityCommand;
 import dev.pgm.community.CommunityPermissions;
 import dev.pgm.community.moderation.feature.ModerationFeature;
 import dev.pgm.community.moderation.punishments.PunishmentType;
+import dev.pgm.community.users.feature.UsersFeature;
 import dev.pgm.community.utils.CommandAudience;
 
 public class WarnCommand extends CommunityCommand {
 
   @Dependency private ModerationFeature moderation;
+  @Dependency private UsersFeature usernames;
 
   @CommandAlias("warn|w")
   @Description("Warn a player for bad behavior")
   @Syntax("[player] [reason]")
   @CommandCompletion("@players *")
   @CommandPermission(CommunityPermissions.WARN)
-  public void warn(CommandAudience sender, OnlinePlayer target, String reason) {
-    moderation.punish(
-        PunishmentType.WARN,
-        target.getPlayer().getUniqueId(),
-        sender,
-        reason,
-        null,
-        false,
-        isVanished(sender));
+  public void warn(CommandAudience audience, String target, String reason) {
+    getTarget(target, usernames)
+        .thenAccept(
+            id -> {
+              if (id.isPresent()) {
+                moderation.punish(
+                    PunishmentType.WARN,
+                    id.get(),
+                    audience,
+                    reason,
+                    null,
+                    false,
+                    isVanished(audience));
+              } else {
+                audience.sendWarning(formatNotFoundComponent(target));
+              }
+            });
   }
 }
