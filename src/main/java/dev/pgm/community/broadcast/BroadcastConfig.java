59f4e34df9180b5a735d038f90c704baee80b232
applenick <applenick@users.noreply.github.com>
2021-02-14 00:32:09 -0800

    Add announcement broadcasts
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- src/main/java/dev/pgm/community/broadcast/BroadcastConfig.java
+++ src/main/java/dev/pgm/community/broadcast/BroadcastConfig.java
@@ -1,15 +1,22 @@
 package dev.pgm.community.broadcast;
 
 import dev.pgm.community.feature.config.FeatureConfigImpl;
+import java.util.List;
 import org.bukkit.configuration.Configuration;
 
 public class BroadcastConfig extends FeatureConfigImpl {
 
   private static final String KEY = "broadcast";
+  private static final String SUB_KEY = "announcements";
 
   private String prefix;
   private boolean sound;
   private int titleSeconds;
+
+  private boolean announceEnabled;
+  private int announceDelay;
+  private String announcePrefix;
+  private List<String> announceMessages;
 
   public BroadcastConfig(Configuration config) {
     super(KEY, config);
@@ -27,11 +34,36 @@ public class BroadcastConfig extends FeatureConfigImpl {
     return titleSeconds;
   }
 
+  public boolean isAnnounceEnabled() {
+    return announceEnabled;
+  }
+
+  public int getAnnounceDelay() {
+    return announceDelay;
+  }
+
+  public String getAnnouncePrefix() {
+    return announcePrefix;
+  }
+
+  public List<String> getAnnounceMessages() {
+    return announceMessages;
+  }
+
+  private String getAnnounceKey() {
+    return getKey() + "." + SUB_KEY;
+  }
+
   @Override
   public void reload(Configuration config) {
     super.reload(config);
     this.prefix = config.getString(getKey() + ".prefix");
     this.sound = config.getBoolean(getKey() + ".sound");
     this.titleSeconds = config.getInt(getKey() + ".title-seconds");
+
+    this.announceEnabled = config.getBoolean(getAnnounceKey() + ".enabled");
+    this.announceDelay = config.getInt(getAnnounceKey() + ".delay-seconds");
+    this.announcePrefix = config.getString(getAnnounceKey() + ".prefix");
+    this.announceMessages = config.getStringList(getAnnounceKey() + ".messages");
   }
 }
--- src/main/java/dev/pgm/community/broadcast/BroadcastFeature.java
+++ src/main/java/dev/pgm/community/broadcast/BroadcastFeature.java
@@ -4,20 +4,71 @@ import static net.kyori.adventure.text.Component.text;
 import static tc.oc.pgm.util.bukkit.BukkitUtils.colorize;
 
 import com.google.common.collect.Sets;
+import dev.pgm.community.Community;
 import dev.pgm.community.CommunityCommand;
 import dev.pgm.community.feature.FeatureBase;
 import dev.pgm.community.utils.BroadcastUtils;
 import dev.pgm.community.utils.Sounds;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.Configuration;
 
 /** BroadcastFeature - Send announcements to inform players about things * */
 public class BroadcastFeature extends FeatureBase {
 
+  private int announceTask;
+
+  private Instant lastAnnounce;
+  private int lastAnnounceIndex;
+
   public BroadcastFeature(Configuration config, Logger logger) {
     super(new BroadcastConfig(config), logger);
+    this.lastAnnounce = Instant.now();
+    this.lastAnnounceIndex = 0;
+
+    if (getConfig().isEnabled()) {
+      enable();
+    }
+  }
+
+  @Override
+  public void enable() {
+    super.enable();
+    startAnnounceTask();
+  }
+
+  @Override
+  public void disable() {
+    super.disable();
+    Bukkit.getScheduler().cancelTask(announceTask);
+  }
+
+  private void startAnnounceTask() {
+    this.announceTask =
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(Community.get(), this::announce, 0L, 20L);
+  }
+
+  // TODO: Allow for /toggle tips once shared settings implemented
+  private void announce() {
+    if (!getBroadcastConfig().isAnnounceEnabled()) return;
+    if (getBroadcastConfig().getAnnounceMessages().isEmpty()) return;
+    Duration timeSince = Duration.between(lastAnnounce, Instant.now());
+    List<String> messages = getBroadcastConfig().getAnnounceMessages();
+
+    if (timeSince.getSeconds() > getBroadcastConfig().getAnnounceDelay()) {
+      if (lastAnnounceIndex >= messages.size()) lastAnnounceIndex = 0;
+      String msg = messages.get(lastAnnounceIndex);
+      Component prefix = text(colorize(getBroadcastConfig().getAnnouncePrefix()));
+      Component message = text(colorize(msg));
+      BroadcastUtils.sendGlobalMessage(text().append(prefix).append(message).build());
+      lastAnnounceIndex++;
+      lastAnnounce = Instant.now();
+    }
   }
 
   public BroadcastConfig getBroadcastConfig() {
