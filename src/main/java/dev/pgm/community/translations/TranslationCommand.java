29ba3e54e843123ffcd73e441d72163a54e66714
applenick <applenick@users.noreply.github.com>
2021-09-13 17:56:45 -0700

    Add /languages to view online language distribution
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- src/main/java/dev/pgm/community/translations/TranslationCommand.java
+++ src/main/java/dev/pgm/community/translations/TranslationCommand.java
@@ -4,15 +4,23 @@ import static net.kyori.adventure.text.Component.text;
 
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Dependency;
 import co.aikar.commands.annotation.Description;
 import dev.pgm.community.CommunityCommand;
 import dev.pgm.community.CommunityPermissions;
 import dev.pgm.community.utils.CommandAudience;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.entity.Player;
+import tc.oc.pgm.util.named.NameStyle;
+import tc.oc.pgm.util.text.PlayerComponent;
+import tc.oc.pgm.util.text.TextFormatter;
 
 public class TranslationCommand extends CommunityCommand {
 
@@ -51,5 +59,49 @@ public class TranslationCommand extends CommunityCommand {
                                 .build());
                       });
             });
+  }
+
+  @CommandAlias("languages")
+  @Description("View a list of online languages")
+  @CommandPermission(CommunityPermissions.TRANSLATE)
+  public void languages(CommandAudience audience, @Default("false") boolean minimal) {
+    Set<String> languages = translate.getOnlineLanguages();
+    audience.sendMessage(
+        text()
+            .append(text("Online Languages", NamedTextColor.DARK_GREEN, TextDecoration.BOLD))
+            .append(text(": ("))
+            .append(text(languages.size(), NamedTextColor.GREEN))
+            .append(text(")"))
+            .color(NamedTextColor.GRAY)
+            .build());
+
+    if (minimal) {
+      audience.sendMessage(
+          TextFormatter.list(
+              languages.stream()
+                  .map(code -> text(code, NamedTextColor.YELLOW))
+                  .collect(Collectors.toList()),
+              NamedTextColor.GRAY));
+      return;
+    }
+
+    for (String language : languages) {
+      List<Player> online = translate.getOnline(language);
+      audience.sendMessage(
+          text()
+              .color(NamedTextColor.GRAY)
+              .append(text(" - "))
+              .append(text(language, NamedTextColor.YELLOW))
+              .append(text(": ("))
+              .append(text(online.size(), NamedTextColor.GOLD))
+              .append(text(") "))
+              .append(
+                  TextFormatter.list(
+                      online.stream()
+                          .map(player -> PlayerComponent.player(player, NameStyle.FANCY))
+                          .collect(Collectors.toList()),
+                      NamedTextColor.GRAY))
+              .build());
+    }
   }
 }
--- src/main/java/dev/pgm/community/translations/TranslationFeature.java
+++ src/main/java/dev/pgm/community/translations/TranslationFeature.java
@@ -1,13 +1,18 @@
 package dev.pgm.community.translations;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Sets;
 import dev.pgm.community.CommunityCommand;
 import dev.pgm.community.feature.FeatureBase;
 import dev.pgm.community.utils.PGMUtils;
 import dev.pgm.community.utils.WebUtils;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Player;
 import tc.oc.pgm.api.integration.Integration;
@@ -38,6 +43,18 @@ public class TranslationFeature extends FeatureBase {
   @Override
   public Set<CommunityCommand> getCommands() {
     return Sets.newHashSet(new TranslationCommand());
+  }
+
+  public Set<String> getOnlineLanguages() {
+    return Bukkit.getOnlinePlayers().stream()
+        .map(Translation::getPlayerLanguageCode)
+        .collect(Collectors.toSet());
+  }
+
+  public List<Player> getOnline(String language) {
+    return Bukkit.getOnlinePlayers().stream()
+        .filter(p -> Translation.getPlayerLanguageCode(p).equalsIgnoreCase(language))
+        .collect(Collectors.toList());
   }
 
   public CompletableFuture<Translation> translate(Player sender, String message) {
