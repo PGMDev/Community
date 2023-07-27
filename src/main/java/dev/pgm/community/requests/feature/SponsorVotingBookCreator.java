3d637ac05420722723da49a6dc57347752142ded
applenick <applenick@users.noreply.github.com>
2023-07-27 11:24:06 -0700

    Simplify voting book creator
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- src/main/java/dev/pgm/community/requests/feature/SponsorVotingBookCreator.java
+++ src/main/java/dev/pgm/community/requests/feature/SponsorVotingBookCreator.java
@@ -2,26 +2,18 @@ package dev.pgm.community.requests.feature;
 
 import static net.kyori.adventure.text.Component.newline;
 import static net.kyori.adventure.text.Component.text;
-import static net.kyori.adventure.text.event.ClickEvent.runCommand;
-import static net.kyori.adventure.text.event.HoverEvent.showText;
 import static tc.oc.pgm.util.player.PlayerComponent.player;
 
 import dev.pgm.community.requests.SponsorRequest;
-import java.util.stream.Collectors;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.ComponentLike;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import tc.oc.pgm.api.map.MapInfo;
-import tc.oc.pgm.api.map.MapTag;
 import tc.oc.pgm.api.player.MatchPlayer;
-import tc.oc.pgm.rotation.vote.book.VotingBookCreator;
+import tc.oc.pgm.rotation.vote.book.VotingBookCreatorImpl;
 import tc.oc.pgm.util.named.NameStyle;
 
-public class SponsorVotingBookCreator implements VotingBookCreator {
-
-  private static final String SYMBOL_IGNORE = "\u2715"; // ✕
-  private static final String SYMBOL_VOTED = "\u2714"; // ✔
+public class SponsorVotingBookCreator extends VotingBookCreatorImpl {
 
   private final RequestFeature manager;
 
@@ -29,39 +21,22 @@ public class SponsorVotingBookCreator implements VotingBookCreator {
     this.manager = manager;
   }
 
-  private Component getHover(MapInfo map) {
-    TextComponent.Builder hover = text();
-    // Map tags
-    hover.append(
-        text(
-            map.getTags().stream().map(MapTag::toString).collect(Collectors.joining(" ")),
-            NamedTextColor.YELLOW));
+  @Override
+  public ComponentLike getHover(MatchPlayer viewer, MapInfo map, boolean voted) {
+    ComponentLike originalHover = super.getHover(viewer, map, voted);
 
-    // Add sponsor hover
+    // Add sponsor hover if available
     SponsorRequest sponsor = manager.getCurrentSponsor();
     if (sponsor != null && sponsor.getMap().equals(map)) {
-      hover
-          .append(
-              text()
-                  .append(newline())
-                  .append(text("+ ", NamedTextColor.GREEN, TextDecoration.BOLD))
-                  .append(text("Sponsored by ", NamedTextColor.GRAY)))
-          .append(player(sponsor.getPlayerId(), NameStyle.FANCY));
+      return text()
+          .append(originalHover)
+          .append(newline())
+          .append(text("+ ", NamedTextColor.GREEN, TextDecoration.BOLD))
+          .append(text("Sponsored by ", NamedTextColor.GRAY))
+          .append(player(sponsor.getPlayerId(), NameStyle.FANCY))
+          .build();
     }
-    return hover.build();
-  }
 
-  @Override
-  public Component getMapBookComponent(MatchPlayer viewer, MapInfo map, boolean voted) {
-    TextComponent.Builder text = text();
-    text.append(
-        text(
-            voted ? SYMBOL_VOTED : SYMBOL_IGNORE,
-            voted ? NamedTextColor.DARK_GREEN : NamedTextColor.DARK_RED));
-    text.append(text(" ").decoration(TextDecoration.BOLD, !voted));
-    text.append(text(map.getName(), NamedTextColor.GOLD, TextDecoration.BOLD));
-    text.hoverEvent(showText(getHover(map)));
-    text.clickEvent(runCommand("/votenext -o " + map.getName())); // Fix 1px symbol diff
-    return text.build();
+    return originalHover;
   }
 }
