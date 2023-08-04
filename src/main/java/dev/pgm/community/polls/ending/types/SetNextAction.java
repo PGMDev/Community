eaff927e46296f299603433bf4a64c1de58fa38d
applenick <applenick@users.noreply.github.com>
2023-08-03 18:00:29 -0700

    Add map tags hover to map poll
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- src/main/java/dev/pgm/community/polls/ending/types/SetNextAction.java
+++ src/main/java/dev/pgm/community/polls/ending/types/SetNextAction.java
@@ -3,12 +3,14 @@ package dev.pgm.community.polls.ending.types;
 import static net.kyori.adventure.text.Component.text;
 
 import dev.pgm.community.polls.ending.EndAction;
+import java.util.stream.Collectors;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.entity.Player;
 import tc.oc.pgm.api.PGM;
 import tc.oc.pgm.api.map.MapInfo;
+import tc.oc.pgm.api.map.MapTag;
 import tc.oc.pgm.util.named.MapNameStyle;
 
 public class SetNextAction implements EndAction {
@@ -36,11 +38,23 @@ public class SetNextAction implements EndAction {
     return map.getStyledName(MapNameStyle.COLOR);
   }
 
+  private Component getFancyMapName() {
+    // TODO: add gamemode once PGM#1210 is merged
+    Component tags =
+        text(
+            map.getTags().stream().map(MapTag::toString).collect(Collectors.joining(" ")),
+            NamedTextColor.YELLOW);
+    return text()
+        .append(map.getStyledName(MapNameStyle.PLAIN).color(NamedTextColor.AQUA))
+        .hoverEvent(HoverEvent.showText(tags))
+        .build();
+  }
+
   @Override
   public Component getDefaultQuestion() {
     return text()
         .append(text("Should we set next "))
-        .append(map.getStyledName(MapNameStyle.PLAIN).color(NamedTextColor.AQUA))
+        .append(getFancyMapName())
         .append(text("?"))
         .color(NamedTextColor.WHITE)
         .build();
