8cbf3c3941a1b92415bfe7573e546f3f251e35a7
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2026-01-26 08:29:26 -0800

    Publish util module
    Signed-off-by: BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>

--- util/build.gradle.kts
+++ util/build.gradle.kts
@@ -1,7 +1,28 @@
 plugins {
     id("buildlogic.java-conventions")
+    `maven-publish`
 }
 
 dependencies {
     compileOnly("dev.pgm.paper:paper-api:1.8_1.21.10-SNAPSHOT")
+}
+
+publishing {
+    publications.create<MavenPublication>("community") {
+        groupId = project.group as String
+        artifactId = project.name
+        version = project.version as String
+
+        artifact(tasks["jar"])
+    }
+    repositories {
+        maven {
+            name = "ghPackages"
+            url = uri("https://maven.pkg.github.com/PGMDev/Community")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
